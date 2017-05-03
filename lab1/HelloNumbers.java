b7880eb27033c8f6e6b2fdf04bc85e28cc601102
WAGN DONG <kelvinson@foxmail.com>
2017-05-04 06:09:51 +0800

    Completed lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers{
+    public static void main(String[] args) {
+        int x = 0;
+        int accum = 0;
+        while (x < 10) {
+             accum = accum + x;
+            System.out.print(accum +" ");
+            x = x + 1;
+        }
+    }
+}ls 
